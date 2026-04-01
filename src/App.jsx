f44b9edac50ee0f3ad5ff9011c5115168c27f84f
H82M19\Student <Student>
2026-04-01 09:57:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+import { Routes,Route,Link } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact'>Contact</Link>
+    </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}>Home</Route>
+        <Route path='/about' element={<About/>}>About</Route>
+        <Route path='/contact' element={<Contact/>}>Contact</Route>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
