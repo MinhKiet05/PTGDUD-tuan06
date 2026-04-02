e331b6fff4983e9a6c1717d7761149d6965858ae
MinhKiet05 <tranvanbetu1965@gmail.com>
2026-04-02 17:40:30 +0700

    Xong cau 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import About from './components/About'
 import Contact from './components/Contact'
 import NotFound from './components/NotFound'
 import Product from './components/Product'
-import { Routes,Route,Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Orders from './components/Orders'
+import Profile from './components/Profile'
+import Settings from './components/Settings'
+
 
 function App() {
 
@@ -14,13 +19,19 @@ function App() {
     <nav>
       <Link to='/'>Home</Link> | 
       <Link to='/about'>About</Link> | 
-      <Link to='/contact'>Contact</Link> |  
+        <Link to='/contact'>Contact</Link> |  
+        <Link to = '/dashboard'>Dashboard</Link>
     </nav>
       <Routes>
-        <Route path='/' element={<Home/>}>Home</Route>
-        <Route path='/about' element={<About/>}>About</Route>
-        <Route path='/contact' element={<Contact/>}>Contact</Route>
-        <Route path='/products/:id' element={<Product/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/products/:id' element={<Product />}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}>
+          <Route path='orders' element={<Orders></Orders>}/>
+          <Route path='profile' element={<Profile></Profile>}/>
+          <Route path='settings' element={<Settings></Settings>}/>
+        </Route>
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
     </>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Link, Outlet } from 'react-router-dom'
 export default function Dashboard() {
   return (
     <div>
-      
+      <h1>Dashboard</h1>
+      <nav>
+        <Link to='orders'>Orders</Link> | 
+        <Link to='profile'>Profile</Link> | 
+        <Link to='settings'>Settings</Link>
+      </nav>
+      <Outlet></Outlet>
+
     </div>
   )
 }
